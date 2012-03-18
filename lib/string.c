551cfdbdcd87ab6282bf4345fbffb71aae7d3d53
Hawx Chen <yingjheng.chen@gmail.com>
2012-03-18 23:32:27 +0800

    Revert "Revert "hand made to format lib/string.c, rever kern/kdebug.c""
    This reverts commit b6b790e9f51edec5efe813685cbe1faa65644803.

--- lib/string.c
+++ lib/string.c
@@ -146,22 +146,31 @@ memmove (void *dst, const void *src, size_t n)
         s += n;
         d += n;
         if ((int) s % 4 == 0 && (int) d % 4 == 0 && n % 4 == 0)
+        {
             asm volatile ("std; rep movsl\n"::"D" (d - 4), "S" (s - 4),
                           "c" (n / 4):"cc", "memory");
+        }
         else
-        asm volatile ("std; rep movsb\n"::"D" (d - 1), "S" (s - 1),
-                      "c" (n):"cc", "memory");
+        {
+            asm volatile ("std; rep movsb\n"::"D" (d - 1), "S" (s - 1),
+                          "c" (n):"cc", "memory");
+        }
         // Some versions of GCC rely on DF being clear
         asm volatile ("cld":::"cc");
     }
     else
     {
         if ((int) s % 4 == 0 && (int) d % 4 == 0 && n % 4 == 0)
+        {
             asm volatile ("cld; rep movsl\n"::"D" (d), "S" (s),
                           "c" (n / 4):"cc", "memory");
+        }
         else
-        asm volatile ("cld; rep movsb\n"::"D" (d), "S" (s), "c" (n):"cc",
-                      "memory");
+        {
+
+            asm volatile ("cld; rep movsb\n"::"D" (d), "S" (s), "c" (n):"cc",
+                          "memory");
+        }
     }
     return dst;
 }
