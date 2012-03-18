104709d3a8148b97ac8a7c23d9fc5d4504299135
Hawx Chen <yingjheng.chen@gmail.com>
2012-03-19 00:05:40 +0800

    Exercise8: just print octal number. Complete

--- lib/printfmt.c
+++ lib/printfmt.c
@@ -216,10 +216,10 @@ vprintfmt (void (*putch) (int, void *), void *putdat, const char *fmt,
             // (unsigned) octal
         case 'o':
             // Replace this with your code.
-            putch ('X', putdat);
-            putch ('X', putdat);
-            putch ('X', putdat);
-            break;
+            num = getuint (&ap, lflag);
+            base = 8;
+            goto number;
+
 
             // pointer
         case 'p':
