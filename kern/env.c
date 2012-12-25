89a48fe501f2c6db2522f70f77b9e7e492ccb893
Hawx Chen <yingjheng.chen@gmail.com>
2012-12-25 18:59:01 +0800

    Exercise04 is done

--- kern/env.c
+++ kern/env.c
@@ -680,6 +680,7 @@ env_run (struct Env *e)
     curenv->env_status = ENV_RUNNING;
     curenv->env_runs++;
     lcr3 (PADDR (curenv->env_pgdir));
+    unlock_kernel();
     env_pop_tf (&curenv->env_tf);
 
     panic ("env_run not yet implemented");
